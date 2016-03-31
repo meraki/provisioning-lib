de383cb140bf4ee54ec0c23cc1b2a8a3b711e676
Alex Pierson <apierson27@gmail.com>
2016-03-31 16:02:26 -0700

    Lint admins

--- admins_test.py
+++ admins_test.py
@@ -1,23 +1,24 @@
 import pytest
 import meraki_admins
 
-with open('key', 'r') as key:
-	test_connector = meraki_admins.DashboardAdmins("524681", key.read().strip())
+with open('key', 'r') as api_key:
+    TEST_CONNECTOR = meraki_admins.DashboardAdmins("524681",
+                                                   api_key.read().strip())
 
 
 def test_add_valid():
-	valid_users = [
-					{"name": "test 1", "email": "test1@test.lol",
-					 "orgAccess": "read-only"}]
-	for user in valid_users:
-		test_request = test_connector.add_admin(user["name"], user["email"],
-										 	 	user["orgAccess"])
-		user_data = test_request.json()
-		unhashables = {}
+    valid_users = [
+        {"name": "test 1", "email": "test1@test.lol",
+         "orgAccess": "read-only"}]
+    for user in valid_users:
+        test_request = TEST_CONNECTOR.add_admin(user["name"], user["email"],
+                                                user["orgAccess"])
+        user_data = test_request.json()
+        unhashables = {}
 
-		for key in user_data.keys():
-			if isinstance(user_data[key], list):
-				unhashables[key] = user_data[key]
-				user_data.pop(key)
+        for key in user_data.keys():
+            if isinstance(user_data[key], list):
+                unhashables[key] = user_data[key]
+                user_data.pop(key)
 
-		assert set(user.items()).issubset(set(user_data.items())) is True
+        assert set(user.items()).issubset(set(user_data.items())) is True
