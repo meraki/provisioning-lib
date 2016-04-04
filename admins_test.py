04389980b25cbdad0d370e67982bf4dabd2e8264
Alex Pierson <apierson27@gmail.com>
2016-04-04 13:22:10 -0700

    Add test for delting existing users

--- admins_test.py
+++ admins_test.py
@@ -5,24 +5,25 @@ with open('key', 'r') as api_key:
     TEST_CONNECTOR = meraki_admins.DashboardAdmins("524681",
                                                    api_key.read().strip())
 
+VALID_USERS = [
+    {"name": "test 1", "email": "test1@test.foo", "orgAccess": "read-only"},
+    {"name": "test 2", "email": "test2@test.foo", "orgAccess": "none",
+     "tags": [{"tag": "test tag", "access": "read-only"}]},
+    {"name": "test 3", "email": "test3@test.foo", "orgAccess": "none",
+     "networks": [{"access": "read-only", "id": "N_629378047925034734"}]},
+    {"name": "test 4", "email": "test4@test.foo", "orgAccess": "none",
+     "networks": [{"access": "monitor-only", "id": "N_629378047925034734"}],
+     "tags": [{"tag": "another one", "access":"guest-ambassador"},
+              {"tag": "last try", "access":"full"}]}
+    ]
+
 
 def test_add_valid():
     """Testing all valid add conditions for users. Assert Dashboard returns HTTP
         201 per the API documentation for adding users, and that all submitted
-        user parameters are returned."""
+        user parameters are returned unmodified."""
 
-    valid_users = [
-        {"name": "test 1", "email": "test1@test.foo", "orgAccess": "read-only"},
-        {"name": "test 2", "email": "test2@test.foo", "orgAccess": "none",
-         "tags": [{"tag": "test tag", "access": "read-only"}]},
-        {"name": "test 3", "email": "test3@test.foo", "orgAccess": "none",
-         "networks": [{"access": "read-only", "id": "N_629378047925034734"}]},
-        {"name": "test 4", "email": "test4@test.foo", "orgAccess": "none",
-         "networks": [{"access": "monitor-only", "id": "N_629378047925034734"}],
-         "tags": [{"tag": "another one", "access":"guest-ambassador"},
-                  {"tag": "last try", "access":"full"}]}
-        ]
-    for user in valid_users:
+    for user in VALID_USERS:
         test_request = TEST_CONNECTOR.add_admin(**user)
         posted_user = test_request.json()
         user_unhashables = {}
@@ -40,6 +41,13 @@ def test_add_valid():
 
         assert (test_request.status_code == 201 and
                 set(user.items()).issubset(set(posted_user.items())) is True)
+
+def test_del_valid():
+    "Testing deleting existing users. Assert Dashboard returns 204."
+
+    for user in VALID_USERS:
+        test_delete = TEST_CONNECTOR.del_admin(user["email"])
+        assert test_delete.status_code == 204
 
 if __name__ == '__main__':
     test_add_valid()
